f22f6e039fcff55d4ab07f16d8dcf7f35f4da2d5
ｙｕｌｉａｏ <166506195+fuffmm@users.noreply.github.com>
2024-10-03 07:43:33 +0300

    added functional javascript :)
    added functional javascript + sidebar nav (WIP)

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ document.addEventListener(
     function saveCheckboxStatus() {
       const checkboxes = document.querySelectorAll('input[type="checkbox"]');
       const checkboxStatus = {};
+      var pct_inc = 0.534759358;
+      var pct = 0;
       var completed = 0;
       checkboxes.forEach(
         checkbox => {
@@ -13,17 +15,23 @@ document.addEventListener(
             parentElement.classList.contains('gives-percent') &&
             checkbox.checked
           ) {
+            pct += pct_inc;
             completed++
           }
         }
       );
-      updateCompletionOverview(completed);
+      // var message_title = 'Checklist Saved';
+      // var message = pct.toFixed(2) + '% Complete';
+      // showToast(message_title, message);
+      updateCompletionOverview(pct, completed);
       localStorage.setItem('checkboxStatus', JSON.stringify(checkboxStatus))
     }
     function loadCheckboxStatus() {
       const checkboxStatus = JSON.parse(localStorage.getItem('checkboxStatus')) ||
       {
       };
+      var pct_inc = 0.534759358;
+      var pct = 0;
       var completed = 0;
       for (const id in checkboxStatus) {
         const checkbox = document.getElementById(id);
@@ -34,15 +42,36 @@ document.addEventListener(
             parentElement.classList.contains('gives-percent') &&
             checkbox.checked
           ) {
+            pct += pct_inc;
             completed++
           }
         }
       }
-      updateCompletionOverview(completed)
+      updateCompletionOverview(pct, completed)
     }
-    function updateCompletionOverview(completed) {
-      document.getElementById('num_completed').innerText = completed;
-      document.getElementById('num_completed_nav').innerText = completed;
+    // function showToast(header, message) {
+    //   const toast = document.getElementById('toast');
+    //   const toast_title = document.getElementById('toast_title');
+    //   const toast_body = document.getElementById('toast_body');
+    //   toast_title.innerText = header;
+    //   toast_body.innerText = message;
+    //   const existingBsToast = bootstrap.Toast.getInstance(toast);
+    //   if (existingBsToast) {
+    //     existingBsToast.dispose()
+    //   }
+    //   const bsToast = new bootstrap.Toast(toast, {
+    //     delay: 5000
+    //   });
+    //   bsToast.show()
+    // }
+    function updateCompletionOverview(pct, completed) {
+      var pct_fixed = pct.toFixed(2);
+      document.getElementById('completion_overview_percent_nav').innerText = pct_fixed + '%';
+      document.getElementById('completion_overview_percent').innerText = pct_fixed + '%';
+      // document.getElementById('completion_progress_bar').style.width = pct_fixed + '%';
+      // document.getElementById('completion_progress_bar').style.width = pct_fixed + '%';
+      document.getElementById('num_completed_nav').innerText = completed
+      document.getElementById('num_completed').innerText = completed
     }
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
     checkboxes.forEach(
@@ -50,6 +79,41 @@ document.addEventListener(
         checkbox.addEventListener('change', saveCheckboxStatus)
       }
     );
+    // var checkAllGroups = document.querySelectorAll('.check-all-group');
+    // checkAllGroups.forEach(
+    //   function (group) {
+    //     var checkAllLink = group.querySelector('.check-all');
+    //     var uncheckAllLink = group.querySelector('.uncheck-all');
+    //     checkAllLink.addEventListener(
+    //       'click',
+    //       function (event) {
+    //         event.preventDefault();
+    //         checkAll(group);
+    //         saveCheckboxStatus()
+    //       }
+    //     );
+    //     uncheckAllLink.addEventListener(
+    //       'click',
+    //       function (event) {
+    //         event.preventDefault();
+    //         uncheckAll(group);
+    //         saveCheckboxStatus()
+    //       }
+    //     )
+    //   }
+    // );
+    // function checkAll(group) {
+    //   var checkboxes = group.querySelectorAll('.form-check-input');
+    //   checkboxes.forEach(function (checkbox) {
+    //     checkbox.checked = true
+    //   })
+    // }
+    // function uncheckAll(group) {
+    //   var checkboxes = group.querySelectorAll('.form-check-input');
+    //   checkboxes.forEach(function (checkbox) {
+    //     checkbox.checked = false
+    //   })
+    // }
     loadCheckboxStatus()
   }
 );
