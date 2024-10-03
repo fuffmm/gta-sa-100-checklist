2fc5f44b284f9b79c7caadcaf7323bc391cb85d4
ｙｕｌｉａｏ <166506195+fuffmm@users.noreply.github.com>
2024-10-03 14:12:25 +0300

    improved ux some and minor fixes :)

--- main.js
+++ main.js
@@ -49,21 +49,6 @@ document.addEventListener(
       }
       updateCompletionOverview(pct, completed)
     }
-    // function showToast(header, message) {
-    //   const toast = document.getElementById('toast');
-    //   const toast_title = document.getElementById('toast_title');
-    //   const toast_body = document.getElementById('toast_body');
-    //   toast_title.innerText = header;
-    //   toast_body.innerText = message;
-    //   const existingBsToast = bootstrap.Toast.getInstance(toast);
-    //   if (existingBsToast) {
-    //     existingBsToast.dispose()
-    //   }
-    //   const bsToast = new bootstrap.Toast(toast, {
-    //     delay: 5000
-    //   });
-    //   bsToast.show()
-    // }
     function updateCompletionOverview(pct, completed) {
       var pct_fixed = pct.toFixed(2);
       document.getElementById('completion_overview_percent_nav_min').innerText = pct_fixed + '%';
@@ -81,41 +66,41 @@ document.addEventListener(
         checkbox.addEventListener('change', saveCheckboxStatus)
       }
     );
-    // var checkAllGroups = document.querySelectorAll('.check-all-group');
-    // checkAllGroups.forEach(
-    //   function (group) {
-    //     var checkAllLink = group.querySelector('.check-all');
-    //     var uncheckAllLink = group.querySelector('.uncheck-all');
-    //     checkAllLink.addEventListener(
-    //       'click',
-    //       function (event) {
-    //         event.preventDefault();
-    //         checkAll(group);
-    //         saveCheckboxStatus()
-    //       }
-    //     );
-    //     uncheckAllLink.addEventListener(
-    //       'click',
-    //       function (event) {
-    //         event.preventDefault();
-    //         uncheckAll(group);
-    //         saveCheckboxStatus()
-    //       }
-    //     )
-    //   }
-    // );
-    // function checkAll(group) {
-    //   var checkboxes = group.querySelectorAll('.form-check-input');
-    //   checkboxes.forEach(function (checkbox) {
-    //     checkbox.checked = true
-    //   })
-    // }
-    // function uncheckAll(group) {
-    //   var checkboxes = group.querySelectorAll('.form-check-input');
-    //   checkboxes.forEach(function (checkbox) {
-    //     checkbox.checked = false
-    //   })
-    // }
+    var checkAllGroups = document.querySelectorAll('.check-all-group');
+    checkAllGroups.forEach(
+      function (group) {
+        var checkAllLink = group.querySelector('.check-all');
+        var uncheckAllLink = group.querySelector('.uncheck-all');
+        checkAllLink.addEventListener(
+          'click',
+          function (event) {
+            event.preventDefault();
+            checkAll(group);
+            saveCheckboxStatus()
+          }
+        );
+        uncheckAllLink.addEventListener(
+          'click',
+          function (event) {
+            event.preventDefault();
+            uncheckAll(group);
+            saveCheckboxStatus()
+          }
+        )
+      }
+    );
+    function checkAll(group) {
+      var checkboxes = group.querySelectorAll('.form-check-input');
+      checkboxes.forEach(function (checkbox) {
+        checkbox.checked = true
+      })
+    }
+    function uncheckAll(group) {
+      var checkboxes = group.querySelectorAll('.form-check-input');
+      checkboxes.forEach(function (checkbox) {
+        checkbox.checked = false
+      })
+    }
     loadCheckboxStatus()
   }
 );
